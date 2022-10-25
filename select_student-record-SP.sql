b0e1e144939ee1bdb6fb6f2511641ebee42283eb
hsoliu <abeeb.ilias@gmail.com>
2022-10-25 18:30:28 +0100

    Create select_student-record-SP.sql

---
+++ select_student-record-SP.sql
@@ -0,0 +1,22 @@
+USE [studentdb]
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE get_studentInfo
+	-- Add the parameters for the stored procedure here
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * FROM studentInfo_tb;
+END
+GO
