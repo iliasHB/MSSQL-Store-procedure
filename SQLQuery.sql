16ac28eab7823e40ed9a13b480722ac02a04f92f
hsoliu <abeeb.ilias@gmail.com>
2022-10-25 11:16:04 +0100

    Create SQLQuery.sql

---
+++ SQLQuery.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE studentdb
+
+DROP DATABASE studentdb;
+
+DROP TABLE studentInfo_tb; 
+
+CREATE TABLE studentInfo_tb (
+	Firstname varchar(50),
+	Surname varchar(50),
+	Age int,
+	Gender varchar(10),
+);
+
